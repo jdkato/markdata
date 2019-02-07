581efd84ab6653b4f8fe3197ca9683b780421512
Joseph Kato <joseph@jdkato.io>
2019-02-06 17:06:09 -0800

    fix: make pattern less greedy

--- markdata/__init__.py
+++ markdata/__init__.py
@@ -17,7 +17,7 @@ from .directives import DIRECTIVES
 # Where 'directive' is a Python function.
 DEFINITIONS = re.compile(
     r"(?:``)?`(?P<directive>[\w_]+)(?P<arguments>\{.*?\})"
-    + r"(?:\n(?P<content>.+)\n``)?`",
+    + r"(?:\n(?P<content>.+?)\n``)?`",
     re.DOTALL | re.VERBOSE,
 )
 
